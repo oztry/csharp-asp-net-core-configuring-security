b747d3ef50cb9b1f4ed19f60cfa389735435937c
Tomasz Ostrozanski <tomasz.ostrozanski@westrock.com>
2023-02-26 15:02:53 +0100

    Module 2

--- ConferenceTracker/Startup.cs
+++ ConferenceTracker/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace ConferenceTracker
@@ -19,6 +20,7 @@ namespace ConferenceTracker
             Configuration = configuration;
         }
 
+        private readonly string _allowedOrigins = "_allowedOrigins";
         public IConfiguration Configuration { get; }
         public string SecretMessage { get; set; }
 
@@ -26,9 +28,23 @@ namespace ConferenceTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            /*
+                We're going to use the .NET Core CLI (Command Line Interface).
+                In the CLI navigate to the ConferenceTracker directory, not the solution's directory. (You can use the cd command to navigate between directories. Example: cd ConferenceTracker)
+                Enter the command dotnet user-secrets init this sets the secretsId for your project
+                Enter the command dotnet user-secrets set "SecretMessage" "Keep it secret, Keep it safe." this sets a secret with the key "SecretMessage" with a value "Keep it secret, Keep it safe."
+            */
+            SecretMessage = Configuration["SecretMessage"];//Normally, you'd use this to contain things like connection strings. However, since we're using an InMemory database, this is simply being used as an example, and serves no functional purpose.
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("ConferenceTracker"));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddCors(options => 
+                            { 
+                                options.AddPolicy(_allowedOrigins, builder => { builder.WithOrigins("http://pluralsight.com"); }); 
+                            });
+            services.Configure<CookiePolicyOptions>(options => 
+                            { options.CheckConsentNeeded = context => true; options.MinimumSameSitePolicy = SameSiteMode.None; 
+                            });
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddTransient<IPresentationRepository, PresentationRepository>();
@@ -37,14 +53,29 @@ namespace ConferenceTracker
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {            
+            if (env.IsDevelopment())
+            {
+                logger.LogInformation("Environment is in development");
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else 
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
             using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
                 context.Database.EnsureCreated();
 
-            app.UseStaticFiles();
+            app.UseCors(_allowedOrigins);
+            app.UseHttpsRedirection();
 
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
             app.UseRouting();
 
             app.UseAuthentication();
